36d088d3bc3a50cf961da23f7bd64b45b1a40e2a
HaronMan <h.kochbati59@gmail.com>
2025-01-17 15:56:48 +0100

    Passage en version web pour le dev

--- electron/main.js
+++ electron/main.js
@@ -1,22 +1,18 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 1200,
-      height: 800,
-      icon: path.join(__dirname, 'assets', 'icon.ico'),
-      autoHideMenuBar: true,
-      webPreferences: {
-        //preload: path.join(__dirname, 'preload.js')
-      }
-    })
-  
-    win.loadFile(path.join(__dirname, '..', 'forecaster-app', 'dist', 'index.html'))
+app.on('ready', () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, 'assets', 'icon.ico'),
+    autoHideMenuBar: true,
+    webPreferences: {
+      //preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-    win.webContents.openDevTools()
-}
+  win.loadURL('http://localhost:8080')
 
-app.whenReady().then(() => {
-    createWindow()
+  //win.loadFile(path.join(__dirname, '..', 'forecaster-app', 'dist', 'index.html'))
 })
