831ec7869a601c29912c007b567f0cdec281299b
HaronMan <h.kochbati59@gmail.com>
2025-01-17 11:30:32 +0100

    Ajout du logo

--- electron/main.js
+++ electron/main.js
@@ -5,6 +5,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
       width: 1200,
       height: 800,
+      icon: path.join(__dirname, 'assets', 'icon.ico'),
       webPreferences: {
         //preload: path.join(__dirname, 'preload.js')
       }
