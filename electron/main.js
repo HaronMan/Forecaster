b2eeb2214aae459b505faa981005e1abb30d0ce8
HaronMan <h.kochbati59@gmail.com>
2025-01-29 18:50:04 +0100

    Début db

--- electron/main.js
+++ electron/main.js
@@ -3,7 +3,7 @@ const path = require('node:path')
 const Database = require('better-sqlite3')
 const fs = require('fs')
 
-app.on('ready', () => {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -17,4 +17,54 @@ app.on('ready', () => {
 
   win.maximize()
   win.loadFile(path.join(__dirname, '..', 'forecaster-app', 'dist', 'index.html'))
-})
+}
+
+const dbPath = path.join(app.getPath('documents'), 'forecaster.db')
+let db;
+
+function createDB() {
+  if (!fs.existsSync(dbPath)) {
+    db = new Database(dbPath);
+    db.exec(`
+      CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        montant INTEGER NOT NULL,
+        notes TEXT NOT NULL,
+        date TEXT NOT NULL
+      )
+    `);
+  }
+}
+
+app.whenReady().then(() => {
+  createDB();
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Réception des requêtes pour accéder aux données
+ipcMain.handle('get-users', () => {
+  /*
+  const stmt = db.prepare('SELECT * FROM users');
+  return stmt.all();4
+  */
+});
+
+ipcMain.handle('add-user', (event, user) => {
+  /*
+  const stmt = db.prepare('INSERT INTO users (name, email) VALUES (?, ?)');
+  const result = stmt.run(user.name, user.email);
+  return result.lastInsertRowid;
+  */
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
